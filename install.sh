2c3aba755ea63881f48c345e14e899157b1dcdd4
Evgeny Vlasenko <mahnunchik@gmail.com>
2016-08-06 18:45:15 +0700

    added install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    # Install some custom requirements on OS X
+    echo "MacOS!"
+    brew update
+    brew install openssl qt5 python3
+    brew link --force openssl
+    brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/1fa330ce7a067406ff05392dead86cccdc0794bd/Formula/boost.rb
+else
+    # Install some custom requirements on Linux
+    echo "Linux!"
+fi
