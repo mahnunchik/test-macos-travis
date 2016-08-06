62d83bf9c1f92eb85059e8afee747e6532c0c2e9
Evgeny Vlasenko <mahnunchik@gmail.com>
2016-08-06 22:29:40 +0700

    added linux dependencies

--- install.sh
+++ install.sh
@@ -7,11 +7,23 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
     brew upgrade cmake git autoconf automake libtool openssl
     brew install qt5 python3
-    brew link --force openssl
-    brew unlink boost
     # Downgrade boost to 1.58.0
+    brew unlink boost
     brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/1fa330ce7a067406ff05392dead86cccdc0794bd/Formula/boost.rb
 else
     # Install some custom requirements on Linux
     echo "Linux!"
+    sudo apt-get update
+    sudo apt-get install -y --no-install-recommends \
+      ca-certificates \
+      build-essential \
+      cmake \
+      git \
+      doxygen \
+      libboost-all-dev \
+      libssl-dev \
+      autogen \
+      automake \
+      libtool \
+      python3
 fi
