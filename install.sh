37b4aa84e4ceef0d6cca148e5c0b1886950230d1
Evgeny Vlasenko <mahnunchik@gmail.com>
2016-08-06 21:53:22 +0700

    some fixes in install script

--- install.sh
+++ install.sh
@@ -5,9 +5,11 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     # Install some custom requirements on OS X
     echo "MacOS!"
     brew update
-    brew install openssl qt5 python3 autoconf automake libtool 
+    brew upgrade cmake git autoconf automake libtool openssl
+    brew install qt5 python3
     brew link --force openssl
     brew unlink boost
+    # Downgrade boost to 1.58.0
     brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/1fa330ce7a067406ff05392dead86cccdc0794bd/Formula/boost.rb
 else
     # Install some custom requirements on Linux
